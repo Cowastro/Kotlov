204a2dbdf46046e1129d809f81cff27a42887a19
Cowastro <dzianis.hotta@gmail.com>
2026-06-08 12:00:47 +0200

    Fix: Filament\Actions\Action namespace in ReviewsTable

--- app/Filament/Resources/Reviews/Tables/ReviewsTable.php
+++ app/Filament/Resources/Reviews/Tables/ReviewsTable.php
@@ -7,7 +7,7 @@ use Filament\Actions\BulkActionGroup;
 use Filament\Actions\DeleteBulkAction;
 use Filament\Actions\EditAction;
 use Filament\Actions\ViewAction;
-use Filament\Tables\Actions\Action;
+use Filament\Actions\Action;
 use Filament\Tables\Columns\IconColumn;
 use Filament\Tables\Columns\ImageColumn;
 use Filament\Tables\Columns\TextColumn;
